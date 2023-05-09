2129f15661fe029d4b80be1c95b02e5b510a5100
liam-ngo <ngoduc2277@gmail.com>
2023-05-09 16:25:28 +0700

    add profile

--- app/page.jsx
+++ app/page.jsx
@@ -14,6 +14,7 @@ function Home() {
         Promtopia is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
+      <Feed />
     </section>
   );
 }
