e0651ab823088aec9f803d812a496009d3bef3d6
liam-ngo <ngoduc2277@gmail.com>
2023-05-10 01:06:55 +0700

    add delete update prompt function

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,18 +7,30 @@ import Profile from "@components/Profile";
 
 const MyProfile = () => {
   const { data: session } = useSession();
-  const [ posts, setPosts] = useState([]);
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
+  const [posts, setPosts] = useState([]);
+  const router = useRouter();
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
+  };
+  const handleDelete = async (post) => {
+
+  };
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch(`/api/users/${session?.user.id}/posts`);
-      const data = await response.json();
-      setPosts(data);
-    };
-    
-    if(session?.user.id) fetchPosts();
+    try {
+      const fetchPosts = async () => {
+        const response = await fetch(`/api/users/${session?.user.id}/posts`);
+        const data = await response.json();
+        setPosts(data);
+        console.log(data);
+      };
+      if (session?.user.id){
+        fetchPosts();
+        return; 
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
 
   return (
--- components/Profile.jsx
+++ components/Profile.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PromptCard from "./PromptCard";
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
-  console.log(data);
   return (
     <section className="w-full">
       <h1 className="head_text text-left">
@@ -16,7 +15,7 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
             post={post}
             handleEdit={() => handleEdit && handleEdit(post)}
             handleDelete={() => handleDelete && handleDelete(post)}
-          />
+            />
         ))}
       </div>
     </section>
